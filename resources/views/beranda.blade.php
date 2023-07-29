92973a18d73bb16bd3a76f0e3e86272e59febc73
AchmadAF <afdhol79@gmail.com>
2023-07-29 22:22:23 +0700

    update beranda, kontak

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -43,7 +43,60 @@ https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css
     {{-- END --}}
 
 <div class="container pt-4 pb-4">
-    <h2 class="text-center">BUKU SEKOLAH</h2>
+    <h2 class="text-center text-primary">BUKU SEKOLAH</h2>
+    <div class="row">
+      <div class="col-md-6 order-md-1 order-last d-flex justify-content-center">
+        <div class="card border-0 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title fw-bolder text-primary">BUKU ECERAN HARGA TERTINGGI</h5>
+            <p class="card-text">
+              Buku pelajaran sekolah untuk jenjang SD-SMP-SMA dengan Harga Eceran Tertinggi dari pemerintah sesuai dengan SK BSKAP Kemendikbudristek RI No. 020/HP/P/2022
+            </p>
+            <a href="{{url('buku')}}" class="btn btn-primary">Lihat Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 order-first order-md-2">
+        <div class="mb-4 mt-4 d-flex justify-content-center">
+          <div class="bg-image"style="max-width: 10rem;">
+            <img src="img/buku/het/SD/1.png" style="width: 10rem" />
+          </div>
+          <div class="bg-image" style="max-width: 10rem;">
+            <img src="img/buku/het/SMP/1.png"  style="width: 10rem" />
+          </div>
+          <div class="bg-image" style="max-width: 10rem;">
+            <img src="img/buku/het/SMP/1.png"  style="width: 10rem" />
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+  <div class="col-md-6 order-md-1 order-last d-flex justify-content-center">
+    <div class="card border-0 shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title fw-bolder text-primary">BUKU PELAJARAN SEKOLAH SAE SMART</h5>
+        <p class="card-text">
+          Buku pelajaran sekolah dengan Kurikulum Merdeka untuk jenjang SD-SMP-SMA yang disusun oleh Tim Penulis SAE SMART dan diterbitkan oleh CV. SAE GROUP
+        </p>
+        <a href="{{url('buku')}}" class="btn btn-primary">Lihat Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6 order-first order-md-2">
+    <div class="mb-4 mt-4 d-flex justify-content-center">
+      <div class="bg-image"style="max-width: 10rem;">
+        <img src="img/buku/pls/keagamaan/SD/agama (1).png" style="width: 10rem" />
+      </div>
+      <div class="bg-image" style="max-width: 10rem;">
+        <img src="img/buku/pls/mulok/SD/mulok (1).png" style="width: 10rem" />
+      </div>
+      <div class="bg-image" style="max-width: 10rem;">
+        <img src="img/buku/pls/pendamping/SD/pdpp (1).png" style="width: 10rem" />
+      </div>
+    </div>
+</div>
+</div>
 </div>
 
 <div class="pt-4 pb-4 text-center" style="background-color: rgb(41, 145, 235);">
--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-KONTAK
+<div class="container mb-5 mt-5">
+    <h2 class="text-center fs-1 ">KONTAK KAMI</h2>
+    <div class="ratio ratio-16x9 mt-4">
+        <iframe loading="lazy" src="https://maps.google.com/maps?q=Dsn.Khoripan%20Rt.03%20Rw.02%20Ds.Bangsongan%C2%A0%20Kec.%20Kayen%20Kidul%20Kab.%20Kediri&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="Dsn.Khoripan Rt.03 Rw.02 Ds.Bangsongan&nbsp; Kec. Kayen Kidul Kab. Kediri" aria-label="Dsn.Khoripan Rt.03 Rw.02 Ds.Bangsongan&nbsp; Kec. Kayen Kidul Kab. Kediri"></iframe>
+    </div>
+    <div class="row mt-3">
+        <div class="col-6">
+            <b>Alamat</b>
+            <p>Dsn.Khoripan Rt.03 Rw.02 Ds.Bangsongan  Kec. Kayen Kidul Kab. Kediri</p>
+            <b>Telepon</b>
+            <p>0821 4066 6588</p>
+            <b>Jam Kerja</b>
+            <p>Buka : 8:00AM - Tutup : 18:00PM | Sabtu - Minggu : Libur</p>
+        </div>
+        <div class="col-6"></div>
+    </div>
+  </div>
 
 @endsection
