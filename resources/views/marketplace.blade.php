45dd35ffab794b3444ef78472d5be1513be3c7b5
AchmadAF <afdhol79@gmail.com>
2023-08-06 23:14:54 +0700

    update

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     <div class="container">
-        <ul class="nav flex-column" style="max-width: 20gitrem">
+        <ul class="nav flex-column" style="max-width: 20rem">
             <li class="nav-item mb-4"><a href="https://siplah.blibli.com/merchant-detail/SSSM-0001?itemPerPage=40&page=0&merchantId=SSSM-0001" target="_blank" class="nav-link p-0 text-muted"><img src="img/toko/blibli.png" class="img-fluid"></a></li>
             <li class="nav-item mb-4"><a href="https://www.mbizmarket.co.id/p/sae-group-ORM0y/catalog" target="_blank" class="nav-link p-0 text-muted"><img src="img/toko/mbizmarket.png" class="img-fluid"></a></li>
             <li class="nav-item mb-4"><a href="https://siplahtelkom.com/store/138360-sae-group" class="nav-link p-0 text-muted" target="_blank"><img src="img/toko/telkom.png" class="img-fluid"></a></li>
