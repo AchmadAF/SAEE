df3616720e82465faeee201ca7b2cbeeb72f50df
AchmadAF <afdhol79@gmail.com>
2023-08-20 20:58:11 +0700

    update loker

--- resources/views/lowongan.blade.php
+++ resources/views/lowongan.blade.php
@@ -8,8 +8,10 @@
 
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <div class="col-md-6 mt-2 mb-2"><img class="img-fluid" src="img/loker/1. MARKETING.png"></div>
-            <div class="col-md-6 mt-2 mb-2"><img class="img-fluid" src="img/loker/2. PENULIS.png"></div>
+            <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/1. MARKETING.png"></div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/2. PUBLISHING.png"></div>
         </div>
     </div>
 
