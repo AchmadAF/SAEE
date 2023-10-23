6ab88a737048d6340e7f987e955c0c6a8b0e9018
AchmadAF <afdhol79@gmail.com>
2023-10-23 18:30:17 +0700

    update

--- resources/views/lowongan.blade.php
+++ resources/views/lowongan.blade.php
@@ -13,6 +13,12 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/2. PUBLISHING.png"></div>
         </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/3. IT.png"></div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/4. ADMIN.png"></div>
+        </div>
     </div>
 
 @endsection
