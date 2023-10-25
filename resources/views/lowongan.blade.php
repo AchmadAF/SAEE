c5d972d638fe774d965ce44a1ba1070225dbc3de
AchmadAF <afdhol79@gmail.com>
2023-10-25 21:05:45 +0700

    update loker

--- resources/views/lowongan.blade.php
+++ resources/views/lowongan.blade.php
@@ -19,6 +19,9 @@
         <div class="row d-flex justify-content-center">
             <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/4. ADMIN.png"></div>
         </div>
+                <div class="row d-flex justify-content-center">
+            <div class="col-md-6 mt-4 mb-4"><img class="img-fluid" src="img/loker/5. FINANCE.png"></div>
+        </div>
     </div>
 
 @endsection
